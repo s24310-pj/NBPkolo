fedab63a62469e5a203db4723b1bb7ee38f2d48b
s24310-pj <s24310@pjwstk.edu.pl>
2023-06-24 10:12:24 +0200

    dodanie plikow projektu

---
+++ jaz-s24310-nbp/src/main/java/pl/pjatk/jazs24310nbp/JazS24310NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs24310nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24310NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24310NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s24310-nbp/src/test/java/pl/pjatk/jazs24310nbp/JazS24310NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs24310nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24310NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
